e34f56f6b9d1868d9296c1f15e016acfa3ff7640
Alex Hedley <alex.hedley@me.com>
2018-11-10 10:37:43 +0000

    Added data and updated app

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
 var myApp = angular.module('myApp',['diff-match-patch']);
 myApp.controller('myController', function ($scope, $http) {   
 
+    createPivot = () => {
+        if ($scope.webservicesLHS && $scope.webservicesRHS) {
+            var methodsLHS = $scope.selectedWebServiceLHS.methods.map(m => m.name); //$scope.original
+            var dataLHS = methodsLHS.map(m => ({method: m, version: $scope.selectedFileLHS}));
+            var methodsRHS = $scope.selectedWebServiceRHS.methods.map(m => m.name); //$scope.changed
+            var dataRHS = methodsRHS.map(m => ({method: m, version: $scope.selectedFileRHS}));
+            var data = dataLHS.concat(dataRHS);
+            
+            $("#output").pivot(
+                data,
+                {
+                    rows: ["method"],
+                    cols: ["version"]
+                }
+            );
+        }
+    };
+
     $scope.options = {
         editCost: 4,
         interLineDiff: true,
@@ -19,7 +37,8 @@ myApp.controller('myController', function ($scope, $http) {
         }
       };
 
-    $scope.files = ['7.0', '7.1', '7.5', '7.6', '8.0', '8.1', '8.5'];
+    //$scope.files = ['7.0', '7.1', '7.5', '7.6', '8.0', '8.1', '8.5'];
+    $scope.files = ['7.0', '7.1', '8.1.5811.0', '8.5.3025.0'];
     $scope.selectedFileLHS = '';
     $scope.selectedFileRHS = '';
 
@@ -30,9 +49,6 @@ myApp.controller('myController', function ($scope, $http) {
     $scope.changed = {};
 
     $scope.loadFile = (side) => {
-        console.log('loadFile');
-        console.log($scope.selectedFileLHS);
-
         var file = '';
         if (side === 'LHS') {
             file = `data/${$scope.selectedFileLHS}.json`;
@@ -57,6 +73,8 @@ myApp.controller('myController', function ($scope, $http) {
         } else {
             $scope.changed = $scope.selectedWebServiceRHS.methods.map(m => m.name).join('\n');
         }
+
+        createPivot();
     };
 });
 
@@ -66,18 +84,18 @@ myApp.controller('myController', function ($scope, $http) {
 //     diffContainer: ".diff1"
 // });
 
-$(function(){
-    $("#output").pivot(
-        [
-            {method: "EnableBulletin", version: "7.0"},
-            {method: "DisableBulletins", version: "7.1"},
-            {method: "EnableBulletin", version: "7.1"}
-        ],
-        {
-            rows: ["method"],
-            cols: ["version"]
-        }
-    );
- });
+// $(function(){
+//     $("#output").pivot(
+//         [
+//             {method: "EnableBulletin", version: "7.0"},
+//             {method: "DisableBulletins", version: "7.1"},
+//             {method: "EnableBulletin", version: "7.1"}
+//         ],
+//         {
+//             rows: ["method"],
+//             cols: ["version"]
+//         }
+//     );
+//  });
 
  // Or https://github.com/nicolaskruchten/pivottable/issues/208
