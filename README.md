f53f855df2e977dc4ade83b7fcc7ff9d80966d81
Alex Hedley <alex.hedley@me.com>
2018-11-25 10:18:15 +0000

    Added ps parse script, added TMS to v8, updated README and added example json file

--- README.md
+++ README.md
@@ -1,2 +1,103 @@
-# SMPWebServiceDiff
-SMP WebService Diff Tool
+# SMP WebService Diff
+
+![](https://img.shields.io/badge/language-powershell-blue.svg)
+![](https://img.shields.io/badge/language-html-purple.svg)
+![](https://img.shields.io/badge/language-css-purple.svg)
+![](https://img.shields.io/badge/language-javascript-purple.svg)
+![](https://img.shields.io/badge/tag-smp-yellow.svg)
+![](https://img.shields.io/badge/tag-symantec-yellow.svg)
+![](https://img.shields.io/badge/tag-webservice-yellow.svg)
+
+![SMP](images/smp.png) SMP WebService Diff Tool has been created to show a quick and easy difference between versions of the SMP. When a new version is released follow the setup and create a new `json` file, add it to the app and now you can see new WS methods.
+
+---
+
+Example Site: [https://alexhedley.github.io/SMPWebServiceDiff/](https://alexhedley.github.io/SMPWebServiceDiff/)
+
+---
+
+## Setup
+
+Create a Folder with the version number of the SMP you are retrieving the WSDLs from.
+
+Save the WSDLs as XML.
+
+For example
+
+- `http://localhost/Altiris/ASDK.Task/TaskManagementService.asmx?WSDL`
+
+Currently each version hosted has the following.
+
+- CollectionManagementService.xml
+- ItemManagementService.xml
+- ItemModel.xml
+- PatchWorkflowSvc.xml
+- ResourceManagementService.xml
+- ResourceModel.xml
+- ScopingManagementService.xml
+- TaskManagementService.xml
+- TaskServerModel.xml
+
+You could add any others you need. 
+
+Example
+```
+7.1.8280
+    CollectionManagementService.xml
+    ItemManagementService.xml
+ParseSMPWSs.ps1
+```
+
+Run the powershell script [ParseSMPWSs.ps1](data\ParseSMPWSs.ps1) and the number of folders you have created will be output.
+
+i.e. if you have a folder called `7.1.8280` there will be a file in the same directory called `7.1.8280.json`.
+
+Example
+```
+7.1.8280
+7.1.8280.json
+ParseSMPWSs.ps1
+```
+
+The structure of the JSON can be seen with the [example.json](data/example.json) file, or any of the others in the data folder.
+
+These then need adding to the `data` folder of this web app and the [app.js](js\app.js) file needs updateing.
+
+This is because I've built a static site that can be hosted here on [github](https://alexhedley.github.io/SMPWebServiceDiff/)
+
+Add the file name to the array: [app.js](js\app.js#L22)
+
+```js
+$scope.files = ['7.1.8280', '8.0.2298', '8.1.5811.0', '8.5.3025.0'];
+```
+
+---
+
+### Extras
+
+One option is to check the SMP server for `*.asmx` files.
+
+```powershell
+$path = '[Install Drive]:\Program Files\Altiris'
+
+Get-ChildItem -Path $path -Filter *.asmx -Recurse -File -Name| ForEach-Object {
+    $_
+}
+```
+
+But you'd want the script to get this path automatically and not manually change the `$path` depending on which SMP Server you are on.
+
+You could get this using a Reg Key.
+
+Example
+```powershell
+$key = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion'
+(Get-ItemProperty -Path $key -Name ProgramFilesDir).ProgramFilesDir
+```
+
+You need to grab:
+
+```
+HKEY_LOCAL_MACHINE\SOFTWARE\Altiris\AIM
+	ProductVersion	(REG_SZ)
+```
---
+++ data/ParseSMPWSs.ps1
@@ -0,0 +1,211 @@
+﻿<#
+.SYNOPSIS
+  Parse WSDLs
+
+.DESCRIPTION
+  Parse SMP WSDLs to JSON for consumption in a Web App
+
+.PARAMETER <>
+    The x of the .
+
+.INPUTS
+  <Inputs if any, otherwise state None>
+
+.OUTPUTS
+  <Outputs if any, otherwise state None - example: Log file stored in C:\Windows\Temp\<name>.log>
+
+.NOTES
+  Version:        1.0
+  Author:         Alex Hedley
+                  Protirus UK Ltd
+  Creation Date:  08/11/2018
+  Purpose/Change: Initial script development
+  
+.EXAMPLE
+  Call '' to 
+#>
+
+#region Initialisations
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+#endregion Initialisations
+
+#region Declarations
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+$LogFilePath = "C:\Temp"
+$Logfile = $LogFilePath + "\" + $MyInvocation.MyCommand.Name + "_" + (Get-Date -Format yyyy-MM-dd) + ".log"
+
+#endregion Declarations
+
+#region Modules
+#------------------------------------------------------------[Modules]-------------------------------------------------------------
+
+#endregion Modules
+
+#region Functions
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+#region Templates
+
+Function Template-Function
+{
+    <#
+    .PARAMETER input1
+        The 'input1' to do x.
+    
+    .EXAMPLE
+        Template-Function "Input"
+    #>
+
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$input1
+    )
+
+    Begin
+    {
+        $sw = [Diagnostics.Stopwatch]::StartNew()
+
+        Write-Debug ("Starting {0} " -f $MyInvocation.MyCommand)
+
+        #Create an hashtable variable 
+        [hashtable]$Return = @{} 
+        $Return.OutCode = 0
+        $Return.OutData = {}
+        $Return.OutDataShort = ""
+    }
+
+    Process
+    {
+        try
+        {
+        }
+        catch
+        {
+            Write-Error $Error.item(0)
+            $Return.OutCode = 2
+            $Return.OutData = $Error.item(0)
+            $Return.OutDataShort = $Error.item(0)
+        }
+    }
+
+    End
+    {
+        $sw.Stop()
+
+        Write-Debug ("Ending {0} " -f $MyInvocation.MyCommand)
+
+        $outmsg = "Function: Template-Function took '$($sw.Elapsed.TotalSeconds)s' to execute."
+        Write-Host $outmsg
+
+        return $Return
+    }
+}
+
+#endregion Templates
+
+Function Log-Message
+{
+    <#
+    .PARAMETER message
+        The message you wish to add to the Logs.
+    
+    .EXAMPLE
+        Log-Message "Hello"
+    #>
+
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$message
+    )
+
+    Begin
+    {
+        Write-Debug ("Starting {0} " -f $MyInvocation.MyCommand)
+    }
+
+    Process
+    {
+        try
+        {
+            #$message = $string.Trim("$Return.OutCode $Return.OutData $Return.OutDataShort")
+            $LogFileMessage = (Get-Date -Format o) + " " + $env:UserName + " " + $message
+            #Write-Host $LogFileMessage
+            Out-File -FilePath $Logfile -Append -Force -InputObject $LogFileMessage
+        }
+        catch
+        {
+            Write-Error $Error.item(0)
+        }
+    }
+
+    End
+    {
+        Write-Debug ("Ending {0} " -f $MyInvocation.MyCommand)
+    }
+}
+#endregion Functions
+
+#region Helpers
+#------------------------------------------------------------[Helpers]-------------------------------------------------------------
+#endregion Helpers
+
+#region Execution
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+$folders = Get-ChildItem -Path $PSScriptRoot -Directory | Select-Object Name
+
+foreach ($folder in $folders) {
+    $folderPath = Join-Path -Path $PSScriptRoot -ChildPath $folder.Name
+
+    $files = Get-ChildItem -Path $folderPath | Select-Object Name
+
+    $data = New-Object -TypeName psobject 
+    $data | Add-Member -MemberType NoteProperty -Name version -Value $folder.Name
+    $data | Add-Member -MemberType NoteProperty -Name webservices -Value @()
+
+    foreach ($file in $files) {
+        $content = Join-Path -Path $folderPath -ChildPath $file.Name
+
+        [xml]$xml = Get-Content $content
+
+        $url = $xml.definitions.service.port[0].address.location
+
+        $webService = New-Object -TypeName psobject
+        $webService | Add-Member -MemberType NoteProperty -Name name -Value $file.Name
+        $webService | Add-Member -MemberType NoteProperty -Name url -Value $url
+        $webService | Add-Member -MemberType NoteProperty -Name methods -Value @()
+
+        $elements = $xml.definitions.types.schema.element
+
+        foreach ($element in $elements) {
+            $parameters = $element.complexType.sequence.element
+            
+            $method = New-Object -TypeName psobject
+            $method | Add-Member -MemberType NoteProperty -Name name -Value $element.name
+
+            if ($parameters) {
+                $method | Add-Member -MemberType NoteProperty -Name parameters -Value @()
+            }
+            
+            foreach ($parameter in $parameters) {
+                $name = $parameter.name
+                $type = $parameter.type
+
+                $parameter = New-Object -TypeName psobject
+                $parameter | Add-Member -MemberType NoteProperty -Name name $name
+                $parameter | Add-Member -MemberType NoteProperty -Name type $type
+
+                $method.Parameters += $parameter
+            }
+
+            $webService.Methods += $method
+        }
+
+        $data.WebServices += $webService
+    }
+
+    $data | ConvertTo-Json -Depth 6 | Out-File "$($PSScriptRoot)\$($folder.Name).json"
+}
+
+#endregion Execution
